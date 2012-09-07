44bffc375875e9c0b37bb61bf5b4144c1f232fa8
thorsten-k <t.kisner@web.de>
2012-09-07 09:22:09 +0000

    id added

--- jsf/src/main/java/net/sf/gravatarjsf/jsf/Gravatar.java
+++ jsf/src/main/java/net/sf/gravatarjsf/jsf/Gravatar.java
@@ -8,18 +8,28 @@ import net.sf.gravatarjsf.util.GravatarHash;
 @FacesComponent(value="net.sf.gravatarjsf.jsf.Gravatar")
 public class Gravatar extends UINamingContainer 
 {
-	public String size(String input)
-	{
-		return ""+input.length();
-	}
+	private static final String gHostSecure = "https://secure.gravatar.com";
 	
-	public String url(String email)
+	public String urlForMail(String email)
 	{
+		System.out.println("urlForMail "+email);
 		StringBuffer sb = new StringBuffer();
-		sb.append("https").append("://");
-		sb.append("secure.gravatar.com");
+		sb.append(gHostSecure);
 		sb.append("/avatar/");
 		sb.append(GravatarHash.md5Hex(email));
+		sb.append("?s=").append(12);
+		sb.append("&d=identicon");
+		
+		return sb.toString();
+
+	}
+	
+	public String urlForHash(String hash)
+	{
+		StringBuffer sb = new StringBuffer();
+		sb.append(gHostSecure);
+		sb.append("/avatar/");
+		sb.append(hash);
 		sb.append("?s=").append(12);
 		sb.append("&d=identicon");
 		
